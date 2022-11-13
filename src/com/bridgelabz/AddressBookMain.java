f9a77d6a87a70af9a2f187c1aa5c84cbb9ee4dd1
adityabhartib <adityabharti.ab@gmail.com>
2022-11-13 15:48:20 +0530

    Added :uc8_ability_to_search_person_in_a_city_or_state

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -26,7 +26,22 @@ public class AddressBookMain {
                 case 4:
                     addressBook.displayContact();
                     break;
-                case 5:addressBook.searchByState();
+                case 5:
+                    System.out.println("1. Search by City\n2. Search by State\n3. Exit");
+                    System.out.println("Choice :");
+                    choice=sc.nextInt();
+                    switch (choice){
+                        case 1:
+                            addressBook.searchByCity();
+                        break;
+                        case 2:
+                            addressBook.searchByState();
+                            break;
+                        case 3:
+                            System.out.println("Thank you");
+                        default:
+                            System.out.println("Please Select Valid Input");
+                    }
                   break;
                 case 6:
                     System.out.println("Thank You for Using Address Book.");
